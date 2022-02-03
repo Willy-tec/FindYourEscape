82c010c0f3d08235d11ddfcd33e3edc5cc14d4aa
willytec <alown666@gmail.com>
2022-02-03 12:33:08 +0100

    Fixing type

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Login.module.css'
-import prisma from '../lib/prisma'
+import {prisma} from '../lib/prisma'
 
 const Login : NextPage = ()=>{
     return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import prisma from '../lib/prisma'
+import {prisma} from '../lib/prisma'
 
 const Home: NextPage = ({feed}) => {
   console.log(feed)
